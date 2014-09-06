f0ce53ed4a0ee5c9c11f92d91025b3c63ecf3e8f
lslinnet <larslinnet@gmail.com>
2014-09-06 12:36:52 +0200

    Added drush scripts

---
+++ drush/aliases/aliases.drushrc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Default drush aliases.drushrc.php file.
+ */
+
+/**
+ * These are the default configuration so that
+ * everyone can just overwrite the different settings.
+ */
+
+$aliases['loc'] = array(
+  'uri' => 'loc.drupalhagen.dk',
+  'root' => '/vagrant_sites/loc.drupalhagen.dk/htdocs',
+  'remote-host' => 'default',
+);
+
